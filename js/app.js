8b52b8113510dc99625fc4e68471d136355035ba
SeldaA24 <104166002+SeldaA24@users.noreply.github.com>
2023-11-02 18:34:16 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,3 +10,22 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+function captureImage(){
+  if(videoStream) {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imgDataUrl = canvas.toDataURL('image/png');
+    capturedImage.src = canvas.toDataURL('image/png');
+    capturedImage.style.display = 'block';
+
+    // Speichern
+    sessionStorage.setItem('capturedImage', imgDataUrl);
+
+    // Speichern Sie das Bild auch im Cache
+    caches.open('my-cache').then(function(cache) {
+      cache.put('/images/captured_image.png', new Response(imgDataUrl));
+    });
+  }
+}
+
