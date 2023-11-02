b027a3170ac3f05e83e6868c32314196c66e29cc
SeldaA24 <104166002+SeldaA24@users.noreply.github.com>
2023-11-02 18:27:44 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -31,16 +31,14 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response) {
-        
-        caches.open('my-cache').then(function(cache) {
+    caches.open('my-cache').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
+          return response;
         });
-        return response;
       });
     })
   );
 });
-
 
