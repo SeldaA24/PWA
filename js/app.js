a8ef33e23792609f8231c0a519f67c90f05076a1
SeldaA24 <104166002+SeldaA24@users.noreply.github.com>
2023-11-02 14:56:20 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,5 @@
 const installIcon = document.createElement("div");
 installIcon.innerHTML = "&#x2192; Install App";
-
-installIcon.style.position = "fixed";
-installIcon.style.bottom = "20px";
-installIcon.style.right = "20px";
-installIcon.style.background = "blue";
-installIcon.style.color = "white";const installIcon = document.createElement("div");
-installIcon.innerHTML = "&#x2192; Install App";
-
 installIcon.style.position = "fixed";
 installIcon.style.bottom = "20px";
 installIcon.style.right = "20px";
@@ -16,7 +8,6 @@ installIcon.style.color = "white";
 installIcon.style.padding = "10px";
 installIcon.style.borderRadius = "50%";
 installIcon.style.cursor = "pointer";
-
 document.body.appendChild(installIcon);
 
 const container = document.querySelector(".container");
@@ -38,11 +29,11 @@ const coffees = [
     image: "images/coffee4.jpg"
   },
   {
-    name: " Beatae",
+    name: "Beatae",
     image: "images/coffee5.jpg"
   },
   {
-    name: " Vitae",
+    name: "Vitae",
     image: "images/coffee6.jpg"
   },
   {
@@ -58,18 +49,18 @@ const coffees = [
     image: "images/coffee9.jpg"
   }
 ];
+
 const showCoffees = () => {
   let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
+  coffees.forEach(({ name, image }) => {
+    output += `
+      <div class="card">
+        <img class="card--avatar" src=${image} />
+        <h1 class="card--title">${name}</h1>
+        <a class="card--link" href="#">Taste</a>
+      </div>
+    `;
+  });
   container.innerHTML = output;
 };
 
@@ -79,116 +70,11 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+      .then(res => console.log("Service worker registered"))
+      .catch(err => console.log("Service worker not registered", err));
   });
 }
 
-
-
-let deferredPrompt;
-
-window.addEventListener("beforeinstallprompt", (event) => {
- 
-  event.preventDefault();
-  deferredPrompt = event;
-
-  
-  installIcon.style.display = "block";
-});
-
-installIcon.addEventListener("click", () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("Die App wurde installiert.");
-      }
-      deferredPrompt = null;
-      installIcon.style.display = "none"; 
-    });
-  }
-});
-
-
-window.addEventListener("appinstalled", (event) => {
-  installIcon.style.display = "none";
-});
-installIcon.style.padding = "10px";
-installIcon.style.borderRadius = "50%";
-installIcon.style.cursor = "pointer";
-
-document.body.appendChild(installIcon);
-
-const container = document.querySelector(".container");
-const coffees = [
-  {
-    name: "Perspiciatis",
-    image: "images/coffee1.jpg"
-  },
-  {
-    name: "Voluptatem",
-    image: "images/coffee2.jpg"
-  },
-  {
-    name: "Explicabo",
-    image: "images/coffee3.jpg"
-  },
-  {
-    name: "Rchitecto",
-    image: "images/coffee4.jpg"
-  },
-  {
-    name: " Beatae",
-    image: "images/coffee5.jpg"
-  },
-  {
-    name: " Vitae",
-    image: "images/coffee6.jpg"
-  },
-  {
-    name: "Inventore",
-    image: "images/coffee7.jpg"
-  },
-  {
-    name: "Veritatis",
-    image: "images/coffee8.jpg"
-  },
-  {
-    name: "Accusantium",
-    image: "images/coffee9.jpg"
-  }
-];
-const showCoffees = () => {
-  let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
-  container.innerHTML = output;
-};
-
-document.addEventListener("DOMContentLoaded", showCoffees);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
-  });
-}
-
-
-// Event abfangen
-// Event abfangen
 let deferredPrompt;
 
 window.addEventListener("beforeinstallprompt", (event) => {
@@ -206,7 +92,7 @@ installIcon.addEventListener("click", () => {
         console.log("Die App wurde installiert.");
       }
       deferredPrompt = null;
-      installIcon.style.display = "none"; 
+      installIcon.style.display = "none";
     });
   }
-});
+};
