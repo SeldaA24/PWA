37b519675c45495fdfb0a176126e7d76ca250598
SeldaA24 <104166002+SeldaA24@users.noreply.github.com>
2023-11-02 22:25:20 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -64,6 +64,87 @@ if ("serviceWorker" in navigator) {
 } */
 
 
+
+
+      let videoStream;
+   let video = document.getElementById('videoCam');
+   let canvas = document.getElementById('canvas');
+   let capturedImage = document.getElementById('capturedImage');
+
+      function openCam(){
+         let All_mediaDevices=navigator.mediaDevices
+         if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+            console.log("getUserMedia() not supported.");
+            return;
+         }
+         All_mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+         })
+         .then(function(vidStream) {
+            var video = document.getElementById('videoCam');
+            if ("srcObject" in video) {
+               video.srcObject = vidStream;
+            } else {
+               video.src = window.URL.createObjectURL(vidStream);
+            }
+            video.onloadedmetadata = function(e) {
+               video.play();
+            }; // Video als Bild gespeicehrt
+           videoStream = vidStream;
+         })
+         .catch(function(e) {
+            console.log(e.name + ": " + e.message);
+         });
+      }
+     function captureImage() {
+  if (videoStream) {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imgDataUrl = canvas.toDataURL('image/png');
+    capturedImage.src = imgDataUrl;
+    capturedImage.style.display = 'block';
+
+    // Im Local Storage speichern
+    localStorage.setItem('capturedImage', imgDataUrl);
+
+    // Im Cache speichern
+    caches.open('my-cache').then(function (cache) {
+      cache.put('/images/captured_image.png', new Response(imgDataUrl));
+    });
+  }
+}
+
+
+ function stopCam(){
+        if(videoStream) {
+           const tracks = videoStream.getTracks();
+           tracks.forEach(track => track.stop());
+           videoStream = null;
+        }
+     }
+
+ function downloadImage() {
+        if (capturedImage.src) {
+           const a = document.createElement('a');
+           a.href = capturedImage.src;
+           a.download = 'captured_image.png';
+           a.click();
+        }
+     }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(function(error) {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
+     
+   
+
 const container = document.querySelector(".container");
 
 
