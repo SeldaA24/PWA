325ff63fada298809dc4cbcc822ca306f0eacd6a
SeldaA24 <104166002+SeldaA24@users.noreply.github.com>
2023-11-02 16:20:25 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,6 @@
 const container = document.querySelector(".container");
 
-const showCoffees = () => {
-  let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
-  container.innerHTML = output;
-};
 
-document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
