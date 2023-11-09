8b4881c0208107beaf8244993d69d4fb16c7719e
SeldaA24 <104166002+SeldaA24@users.noreply.github.com>
2023-11-09 16:17:29 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -181,7 +181,22 @@ function captureImage(){
 
 
 
+var target = document.getElementById('target');
+var watchId;
 
-<button id="askButton">Ask for location</button>
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
 
-<div id="target"></div>
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
